93ef0a6259300d0d73b811ba3f127b792fca8bd4
Andrés Murillo <andresdavid1305@hotmail.com>
2024-11-14 16:06:25 -0500

    astro to netlify

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_CfF3OxOy.mjs';
+import { manifest } from './manifest_C-vzYOxu.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
@@ -19,7 +19,7 @@ const _manifest = Object.assign(manifest, {
     middleware: undefined
 });
 const _args = {
-    "middlewareSecret": "71905d25-1c05-45b3-8806-a16d17f84caf"
+    "middlewareSecret": "f042b440-061d-4e58-b194-57c50544a3cf"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
